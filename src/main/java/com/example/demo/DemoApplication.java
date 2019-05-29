a98c2df76d078d6ddb81a3989baf0b0455aab880
keisuke.umegaki <keisuke.umegaki@rakuten.com>
2019-05-29 16:09:17 +0900

    Must get TODO done

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	// TODO: MySQL, WebSecurityConfig, TopController 作成
+	// TODO: 環境に応じたDBを作成する必要がある
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
