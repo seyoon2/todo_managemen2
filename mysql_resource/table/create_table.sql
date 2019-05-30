9716e1944a95536dc1e7cd31c24536c0b52b9272
keisuke.umegaki <keisuke.umegaki@rakuten.com>
2019-05-31 08:58:23 +0900

    [Fix] エンティティ修正

--- mysql_resource/table/create_table.sql
+++ mysql_resource/table/create_table.sql
@@ -1,6 +1,6 @@
 USE my_demo
-CREATE TABLE IF NOT EXISTS users (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    accountid varchar(20) NOT NULL,
-    password char(60) NOT NULL
+CREATE TABLE IF NOT EXISTS 'users' (
+    'id' int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    'account_id' varchar(20) NOT NULL,
+    'password' char(60) NOT NULL
 );
