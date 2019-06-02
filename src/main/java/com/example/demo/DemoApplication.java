b92630e6842c4845279db9c4d5237535ae740932
kskumgk63 <apple0630uk@i.softbank.jp>
2019-06-02 13:54:25 +0900

    [Add] 投稿削除機能

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    // TODO 投稿の削除機能
-    // TODO ユーザーの一覧
+    // TODO ユーザーの検索画面
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
--- src/main/java/com/example/demo/controller/PostDeleteController.java
+++ src/main/java/com/example/demo/controller/PostDeleteController.java
@@ -4,7 +4,14 @@ import com.example.demo.service.PostDeleteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Locale;
+import java.util.Objects;
 
 
 /**
@@ -30,10 +37,29 @@ public class PostDeleteController {
         this.messageSource = messageSource;
     }
 
-    // TODO コントローラの実装(存在の有無をチェックなどする)
-
-//    @RequestMapping(value = "", method = RequestMethod.POST)
-//    public delete(@RequestParam String postId, Model model) {
-//
-//    }
+    /**
+     * 削除実行
+     *
+     * @param postId
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String delete(@RequestParam String postId, Model model) {
+        // 遷移元の画面から、削除対象の投稿のIDが渡ってこなければ、エラーとする
+        if (Objects.isNull(postId) || StringUtils.isEmpty(postId)) {
+            model.addAttribute("errorMsg", messageSource.getMessage("validation.invalid.screen.transition", null, Locale.JAPAN));
+            // TODO エラー画面未実装
+            return "common/commonError";
+        }
+        // 投稿が存在しなければ、エラー表示。
+        if (!service.isExistsById(Integer.parseInt(postId))) {
+            model.addAttribute("errorMsg", messageSource.getMessage("validation.incorrect.specification.todo", null, Locale.JAPAN));
+            return "common/commonError";
+        }
+        // 削除処理
+        service.delete(Integer.parseInt(postId));
+        // 残っている投稿を見るためにリダイレクトさせる
+        return "redirect:/top";
+    }
 }
